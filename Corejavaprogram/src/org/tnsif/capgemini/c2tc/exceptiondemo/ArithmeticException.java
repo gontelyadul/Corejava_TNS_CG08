6adba72e3629a6bff2487149e4982f4d2bdd02a4
gontelyadul <gontelyadul@gmail.com>
2026-06-11 11:42:18 +0530

    commit

---
+++ Corejavaprogram/src/org/tnsif/capgemini/c2tc/exceptiondemo/ArithmeticException.java
@@ -0,0 +1,25 @@
+package org.tnsif.capgemini.c2tc.exceptiondemo;
+
+public class ArithmeticException {
+
+	public static void main(String[] args) {
+		int number_one=50/5;
+		System.out.println(number_one);
+		int number_two=50/10;
+		System.out.println(number_two);
+		try {
+			int number_three=50/0;
+			System.out.println(number_three);	
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+		int number_four=50/25;
+		System.out.println(number_four);
+		
+		
+
+	}
+
+}
