8b5189b5b80b96d3c272d452755583b81dd242e9
gontelyadul <gontelyadul@gmail.com>
2026-06-18 12:37:19 +0530

    commit

---
+++ Corejavaprogram/src/com/tnsif/lambdaexpression/LambdaNopara.java
@@ -0,0 +1,13 @@
+package com.tnsif.lambdaexpression;
+
+@FunctionalInterface
+interface Sayble{
+        public String say();
+}
+
+public class LambdaNopara {
+public static void main(String[] args) {
+        Sayble s=()->{return "i have nothing to say.";};
+        System.out.println(s.say());
+}
+}
